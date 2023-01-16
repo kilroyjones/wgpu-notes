28921c38d0e8f76d837e31edf1b52181674bc4cf
kilroyjones <tsbach@gmail.com>
2023-01-16 20:59:01 +0800

    updated

---
+++ code/src/bin/skeleton/main.rs
@@ -0,0 +1,70 @@
+use winit::{
+    // event::{WindowEvent, Event},
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window, WindowBuilder}
+};
+use wgpu::{Instance, Adapter};
+
+pub fn create_window(event_loop: &EventLoop<()>) -> Option<Window> {
+    match WindowBuilder::new().build(&event_loop) { 
+        Ok(window) => Some(window),
+        Err(_) => {
+            // Log error 
+            None 
+        }
+    }
+}
+
+pub async fn get_adapter(instance: &Instance) -> Option<Adapter> {
+    instance
+        .request_adapter(&wgpu::RequestAdapterOptions::default())
+        .await
+}
+
+pub fn handle_window_events(event: WindowEvent<'_>, control_flow: &mut ControlFlow) {
+    match event {
+        WindowEvent::CloseRequested => {
+            *control_flow = ControlFlow::Exit;
+        },
+        _ => {}
+    }
+}
+
+
+pub async fn run() {
+    let event_loop = EventLoop::new();
+    let instance = wgpu::Instance::new(wgpu::Backends::all());
+
+    let window = match create_window(&event_loop) {
+        Some(window) => window, 
+        None => panic!("Unable to create window")
+    };
+    
+    let adapter = match get_adapter(&instance).await {
+        Some(adapter) => {
+            adapter
+        }
+        None => panic!("Unable to find a suitable WPGU adapter")
+    };
+    println!("Adapter loaded: {:?}", adapter);
+
+    event_loop.run(move | event: Event<()>, _, control_flow: &mut ControlFlow | {
+        match event {
+            Event::WindowEvent {
+                event,
+                window_id,
+            } => {
+                if window_id == window.id() { 
+                    handle_window_events(event, control_flow);
+                }
+            }
+            _ => {}
+        };
+    });
+}
+
+fn main() {
+    env_logger::init();
+    pollster::block_on(run());
+}
---
+++ src/SUMMARY.md
@@ -0,0 +1,4 @@
+# Summary
+
+- [Introduction](introduction.md)
+  - [What is WGPU?](introduction/what-is-wgpu.md)
